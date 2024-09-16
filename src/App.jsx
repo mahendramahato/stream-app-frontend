0d24bf1e635b48adbaf488f25e384f0fb33086fc
mahendramahato <mahendramahato33@gmail.com>
2024-09-16 19:27:19 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import VideoUpload from './components/VideoUpload'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="flex flex-col items-center space-y-9 justify-center py-9">
+        <h1 className="text-2xl font-bold text-gray-500"> 
+          Video Streaming App
+        </h1>
+        <VideoUpload />
+      </div>
+      
+    </>
+  )
+}
+
+export default App
